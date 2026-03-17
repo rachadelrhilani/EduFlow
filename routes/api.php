e5de2871a40dfcedad3e80434dd916d4bf867ce3
rachad <rachdos76@gmail.com>
2026-03-17 20:38:48 +0000

    creer les routes Crud api des courses

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CourseController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,7 +11,17 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
 Route::post('/password/forgot', [AuthController::class, 'forgotPassword']);
-
 Route::post('/password/reset', [AuthController::class, 'resetPassword'])->name('password.reset');
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+
+    Route::middleware('role:enseignant')->group(function () {
+        Route::post('/courses', [CourseController::class, 'store']);
+        Route::put('/courses/{id}', [CourseController::class, 'update']);
+        Route::delete('/courses/{id}', [CourseController::class, 'destroy']);
+    });
+});
