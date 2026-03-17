d7ff01ac99494cb8ee03576c450bfb83ccf854c1
rachad <rachdos76@gmail.com>
2026-03-17 20:29:54 +0000

    creer middleware role pour sécurise l'acces selon le role

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next,string $role): Response
+    {
+        // 1. Vérifier si l'utilisateur est connecté
+        $user = auth('api')->user();
+
+        if (!$user) {
+            return response()->json(['message' => 'Non authentifié.'], 401);
+        }
+
+        if ($user->role !== $role) {
+            return response()->json([
+                'message' => "Accès refusé. Vous devez être $role pour effectuer cette action."
+            ], 403);
+        }
+        return $next($request);
+    }
+}
