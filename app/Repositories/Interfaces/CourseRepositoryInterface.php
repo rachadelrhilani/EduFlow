924e2799f75bb6f245b96b508586e395da35ce1e
rachad <rachdos76@gmail.com>
2026-03-17 20:07:41 +0000

    ajouter l'interface pour gere les methodes les courses

---
+++ app/Repositories/Interfaces/CourseRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Repositories\Interfaces;
+
+interface CourseRepositoryInterface {
+    public function all();
+    public function findById(int $id);
+    public function create(array $data);
+    public function update(int $id, array $data);
+    public function delete(int $id);
+    public function getByTeacher(int $teacherId);
+}
